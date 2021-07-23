0c982e0e685e5bbd84a8d3ae4924e1f9eefc9b4e
Charliemur2 <charliemurciamunoz@gmail.com>
2021-07-22 23:19:46 -0500

    create 0-lockboxes.py

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''method that determines if all the boxes can be opened.'''
+
+
+def canUnlockAll(boxes):
+    '''method that determines if all the boxes can be opened.
+        args:
+            boxes: list of lists
+        return:
+            true or false depending if all boxes are oppened or not
+    '''
+    open_boxes = set()
+    keys = set()
+    for i in range(len(boxes)):
+        for j in range(len(boxes[i])):
+            open_boxes.add(i)
+            keys.add(boxes[i][j])
+    if len(keys) == len(open_boxes):
+        return True
+    else:
+        return False
