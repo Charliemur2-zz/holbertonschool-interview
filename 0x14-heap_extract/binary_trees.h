defff19f431632fa5ab742a57a363dafa430df9a
Charliemur2 <charliemurciamunoz@gmail.com>
2022-05-15 15:11:58 -0500

    fix

--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -1,11 +1,6 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-/*Basic Binary Tree*/
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,23 +15,13 @@ struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-
 };
 
+#include <stdlib.h>
+
 typedef struct binary_tree_s binary_tree_t;
-
-/*Basic Binary Tree*/
 typedef struct binary_tree_s heap_t;
-
-/*Mandatory Task Functions*/
-heap_t *_array_to_heap(int *array, size_t size);
-void binary_tree_print(const binary_tree_t *tree);
-void _binary_tree_delete(binary_tree_t *tree);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-size_t binary_tree_size(const binary_tree_t *tree);
-int heap_extract(heap_t **root);
-
-/*Functions printing*/
 void binary_tree_print(const binary_tree_t *);
+int heap_extract(heap_t **root);
 
 #endif /* _BINARY_TREES_H_ */
