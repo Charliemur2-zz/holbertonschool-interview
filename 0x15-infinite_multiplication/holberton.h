99a1843fb6ffb7800cb7b9466327c50b6ebe2e0c
Charliemur2 <charliemurciamunoz@gmail.com>
2022-05-22 14:24:18 -0500

    create 0x15-infinite_multiplication project

--- 0x15-infinite_multiplication/holberton.h
+++ 0x15-infinite_multiplication/holberton.h
@@ -1,15 +1,11 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int _putchar(char c);
-int _isdigit(int c);
-void *_calloc(unsigned int nmemb, unsigned int size);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void _puts(char *str);
-int _strlen(char *s);
 
-#endif
+#endif /* _HOLBERTON_H_ */
